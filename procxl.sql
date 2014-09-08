66f0ee7b647c9833dbb766587c11a3081017fd15
brkrishna <brkrishna@gmail.com>
2014-09-08 17:50:48 +0530

    updated sql file

--- procxl.sql
+++ procxl.sql
@@ -14,7 +14,7 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+-- updated version as of 1800 hours on 8/sep/2014
 --
 -- Table structure for table `wp_commentmeta`
 --
